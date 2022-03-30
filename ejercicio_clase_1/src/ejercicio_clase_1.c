f6ac3773b5f6ea76b33e32864037079d954e74d2
pablo.s.ali@hotmail.com <pablo.s.ali@hotmail.com>
2022-03-30 20:50:00 -0300

    primer commit

---
+++ ejercicio_clase_1/src/ejercicio_clase_1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ejercicio_clase_1.c
+ Author      : Ali, Pablo Sharif
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Bienvenidos a Programación 1-UTN"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
